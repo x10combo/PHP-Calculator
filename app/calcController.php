b402ad5284bc65594e0937ddffad95da0ed76064
x10combo <rakelspahi03@gmail.com>
2023-07-19 15:30:38 +0200

    Fixed a typo

--- app/calcController.php
+++ app/calcController.php
@@ -1,30 +1,29 @@
 <?php require_once "./Calculator.php";
 
-// Upon pressing the submit button:
+// If the submit button has been pressed
+if (isset($_POST['submit'])) {
 
-if(isset($_Post['submit'])) {
-
-    // Upon confirming both number values, a new instance of the Calculator is opened
+    // Check number values
     if (is_numeric($_POST['number1']) && is_numeric($_POST['number2'])) {
         $calculator = new \App\Calculator;
 
-    // Calculating the total based on the selected arithmetic operation, using if-else statements.
-    if ($_POST['operation'] == 'plus') {
-        $total = $calculator -> add($_POST['number 1'], $_POST['number 2']);
-    } else if ($_POST['operation'] == 'minus'){
-        $total = $calculator -> subtract($_POST['number 1'], $_POST['number 2']);
-    } else if ($_POST['operation'] == 'times'){
-        $total = $calculator -> multiply($_POST['number 1'], $_POST['number 2']);
-    } else if ($_POST['operation'] == 'divided by'){
-        $total = $calculator -> divide($_POST['number 1'], $_POST['number 2']);
-    }
+        // Calculate total
+        if ($_POST['operation'] == 'plus') {
+            $total = $calculator->add($_POST['number1'], $_POST['number2']);
+        } else if ($_POST['operation'] == 'minus') {
+            $total = $calculator->subtract($_POST['number1'], $_POST['number2']);
+        } else if ($_POST['operation'] == 'times') {
+            $total = $calculator->multiply($_POST['number1'], $_POST['number2']);
+        } else if ($_POST['operation'] == 'divided by') {
+            $total = $calculator->divide($_POST['number1'], $_POST['number2']);
+        }
 
-    //printing of output message
-    echo "<h1>{$_POST['number1']} {$_POST['operation']} {$_POST['number2']} equals {$total}</h1>";
+        // 
+        echo "<h1>{$_POST['number1']} {$_POST['operation']} {$_POST['number2']} equals {$total}</h1>";
 
-} else {
+    } else {
 
-    // Otherwise prints error message to the browser
-    echo 'Numeric values are required';
+        // Print error message to the browser
+        echo 'Numeric values are required';
     }
 }
